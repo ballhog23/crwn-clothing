a428786e9f5f0f021c8d62540e1b8ca75dc0b64e
Caleb <blakeshawart@gmail.com>
2024-02-10 18:25:44 -0600

    basic structure

--- src/App.js
+++ src/App.js
@@ -2,8 +2,52 @@
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="categories-container">
+      <section className="category-container">
+        {/* <img className="category-image" src="" alt="" loading="lazy" decoding="async" /> */}
+        <article className="category-body-container">
+          <header className="category-header">
+            <h2>Hats</h2>
+          </header>
+          <p>Show Now</p>
+        </article>
+      </section>
+      <section className="category-container">
+        {/* <img className="category-image" src="" alt="" loading="lazy" decoding="async" /> */}
+        <article className="category-body-container">
+          <header className="category-header">
+            <h2>Jackets</h2>
+          </header>
+          <p>Show Now</p>
+        </article>
+      </section>
+      <section className="category-container">
+        {/* <img className="category-image" src="" alt="" loading="lazy" decoding="async" /> */}
+        <article className="category-body-container">
+          <header className="category-header">
+            <h2>Sneakers</h2>
+          </header>
+          <p>Show Now</p>
+        </article>
+      </section>
+      <section className="category-container">
+        {/* <img className="category-image" src="" alt="" loading="lazy" decoding="async" /> */}
+        <article className="category-body-container">
+          <header className="category-header">
+            <h2>Womens</h2>
+          </header>
+          <p>Show Now</p>
+        </article>
+      </section>
+      <section className="category-container">
+        {/* <img className="category-image" src="" alt="" loading="lazy" decoding="async" /> */}
+        <article className="category-body-container">
+          <header className="category-header">
+            <h2>Mens</h2>
+          </header>
+          <p>Show Now</p>
+        </article>
+      </section>
     </div>
   );
 }
