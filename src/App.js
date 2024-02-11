85de78279bfced589d95fb976862f29df346afd4
Caleb <blakeshawart@gmail.com>
2024-02-10 18:33:44 -0600

    updating basic structure to use array of obj instead of hard coding - added images to public

--- src/App.js
+++ src/App.js
@@ -1,53 +1,48 @@
 
 
 const App = () => {
+
+  const categoriesArray = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ]
+
   return (
     <div className="categories-container">
-      <section className="category-container">
-        {/* <img className="category-image" src="" alt="" loading="lazy" decoding="async" /> */}
-        <article className="category-body-container">
-          <header className="category-header">
-            <h2>Hats</h2>
-          </header>
-          <p>Show Now</p>
-        </article>
-      </section>
-      <section className="category-container">
-        {/* <img className="category-image" src="" alt="" loading="lazy" decoding="async" /> */}
-        <article className="category-body-container">
-          <header className="category-header">
-            <h2>Jackets</h2>
-          </header>
-          <p>Show Now</p>
-        </article>
-      </section>
-      <section className="category-container">
-        {/* <img className="category-image" src="" alt="" loading="lazy" decoding="async" /> */}
-        <article className="category-body-container">
-          <header className="category-header">
-            <h2>Sneakers</h2>
-          </header>
-          <p>Show Now</p>
-        </article>
-      </section>
-      <section className="category-container">
-        {/* <img className="category-image" src="" alt="" loading="lazy" decoding="async" /> */}
-        <article className="category-body-container">
-          <header className="category-header">
-            <h2>Womens</h2>
-          </header>
-          <p>Show Now</p>
-        </article>
-      </section>
-      <section className="category-container">
-        {/* <img className="category-image" src="" alt="" loading="lazy" decoding="async" /> */}
-        <article className="category-body-container">
-          <header className="category-header">
-            <h2>Mens</h2>
-          </header>
-          <p>Show Now</p>
-        </article>
-      </section>
+      {
+        categoriesArray.map(({title, id}) => {
+          return (
+            <section className="category-container" key={id}>
+              {/* <img className="category-image" src="" alt="" loading="lazy" decoding="async" /> */}
+              <article className="category-body-container">
+                <header className="category-header">
+                  <h2>{title}</h2>
+                </header>
+                <p>Show Now</p>
+              </article>
+            </section>
+          );
+        })
+      }
     </div>
   );
 }
